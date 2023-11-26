dea830aad5505366a7dc86a91bc00e479ffbc4a5
Kevin Leiva <Kevinomg9@gmail.com>
2023-11-26 19:08:26 -0300

    CV

--- src/Components/Utils/Navbar/index.js
+++ src/Components/Utils/Navbar/index.js
@@ -19,14 +19,14 @@ function Navbar() {
 
 
     return (
-        <div className='w-full flex justify-between max-w-[1200px] 2xl:max-w-[1500px] mx-auto p-5 border-b-2 text-[#171717]'>
+        <div className='w-full flex justify-between items-center max-w-[1200px] 2xl:max-w-[1500px] mx-auto p-5 border-b-2 text-[#171717]'>
             <div className={show == false ? 'hidden' : 'toast'}>
                 <div className="alert alert-success">
                     <span className='text-white'>CV Descargado Correctamente</span>
                 </div>
             </div>
-            <a href='/' className='text-2xl flex justify-center items-center '> <AiFillCode size={40} /> {`Kevin leiva`}</a>
-            <a onClick={handleShow} href={CV} download="Cv_Kevin_leiva" className='btn bg-[#171717] text-white font-normal'>
+            <a href='/' className='text-xl flex justify-center items-center '> <AiFillCode size={40} /> {`Kevin leiva`}</a>
+            <a onClick={handleShow} href={CV} download="Cv_Kevin_leiva" className='btn btn bg-[#171717] text-white font-normal'>
                 <FaFileDownload />
                 Descargar CV
             </a>
